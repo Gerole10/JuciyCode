b87ff15186bbdc9b539c8655ee1e7a8e7b164879
Gerole10 <gerole10@gmail.com>
2019-10-08 17:50:06 +0200

    Añadimos la clase Box para simular el box en el taller

---
+++ src/tallerapedales/Box.java
@@ -0,0 +1,12 @@
+/*
+ * Proyecto realizado por Germán Rolando.
+ */
+package tallerapedales;
+
+/**
+ *
+ * @author Germán
+ */
+public class Box {
+    //Clase para crear el objeto Box para luego simular el taller con varios box
+}
