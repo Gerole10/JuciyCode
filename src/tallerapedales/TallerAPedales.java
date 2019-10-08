a89d1c9a387456b3e1b1628d05c8d3112b72bbfc
Gerole10 <gerole10@gmail.com>
2019-10-08 17:36:59 +0200

    Inicio del proyecto con la clase main

---
+++ src/tallerapedales/TallerAPedales.java
@@ -0,0 +1,19 @@
+/*
+ * Proyecto realizado por Germán Rolando.
+ */
+package tallerapedales;
+
+/**
+ *
+ * @author Germán
+ */
+public class TallerAPedales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
