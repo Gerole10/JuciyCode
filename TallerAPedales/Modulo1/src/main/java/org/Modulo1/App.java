d01558dad669499bf01b515c76eae2a666ffa350
José Otero Aguado <56037375+oteroo99@users.noreply.github.com>
2019-11-20 13:59:37 +0100

    Add files via upload

---
+++ TallerAPedales/Modulo1/src/main/java/org/Modulo1/App.java
@@ -0,0 +1,14 @@
+package org.Modulo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Holiwis esto a ver si se cambia.");
+    }
+}
---
+++ TallerAPedales/Modulo2/src/main/java/org/Modulo2/App.java
@@ -0,0 +1,13 @@
+package org.Modulo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
