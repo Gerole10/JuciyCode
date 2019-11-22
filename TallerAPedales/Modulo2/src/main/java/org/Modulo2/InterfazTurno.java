ad977e9760d3cf12574b89eed01aa4cf5d5e0d54
José Otero Aguado <56037375+oteroo99@users.noreply.github.com>
2019-11-22 20:45:54 +0100

    Version 2.0.1 del Modulo 2

--- TallerAPedales/Modulo2/src/main/java/org/Modulo2/InterfazTurno.java
+++ TallerAPedales/Modulo2/src/main/java/org/Modulo2/InterfazTurno.java
@@ -4,22 +4,20 @@ public class InterfazTurno {
 
 	public void setLibre() {	
 	}
-	
-	public void setReservado() {
-		
+	public void setReservado() {		
 	}
-	public void setOcupado() {
-		
+	public void setOcupado() {	
 	}
-	public void setPidiendo() {
-		
+	public void setPidiendo() {		
 	}
-	public void setEnEsperaDeRespuesta() {
-		
+	public void setEnEsperaDeRespuesta() {	
 	}
-	public void setServidos() {
-		
+	public void setServidos() {		
 	}
-	
-
+	public void setEsperandoLaCuenta() {	
+	}
+	public void setPagando() {	
+	}
+	public void setEnPreparacion() {	
+	}
 }
